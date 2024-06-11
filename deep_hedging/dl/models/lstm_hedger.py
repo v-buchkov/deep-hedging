1c781b7b620f2132ea92da7fe8df81bf8f44b6e4
Vyacheslav Buchkov <104529850+v-buchkov@users.noreply.github.com>
2024-06-11 12:18:31 +0300

    black formatted

--- deep_hedging/dl/models/lstm_hedger.py
+++ deep_hedging/dl/models/lstm_hedger.py
@@ -59,11 +59,11 @@ class LSTMHedger(AbstractHedger):
             h_t, c_t = hidden
 
         mid = (spot[:, :, 0] + spot[:, :, 1]) / 2
+        mid = torch.log(mid).unsqueeze(2)
+
         bid_ask_spread = (spot[:, :, 1] - spot[:, :, 0]).unsqueeze(2)
-
-        mid = torch.log(mid[:, :] / mid[:, 0].unsqueeze(1)).unsqueeze(2) * GlobalConfig.TRADING_DAYS
-
         rates = spot[:, :, 2:4] - spot[:, 0, 2:4].unsqueeze(1)
+
         spot = torch.cat([mid, bid_ask_spread, rates, spot[:, :, 4:]], dim=2)
 
         h_t, c_t = self.lstm(spot, (h_t, c_t))
